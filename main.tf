9e936680f9239ca22271292e5d73b33a3b3bac41
edongsky <143239264+edongsky@users.noreply.github.com>
2023-08-27 14:35:13 +0800

    Vpc id removed

--- main.tf
+++ main.tf
@@ -32,8 +32,6 @@ resource "aws_instance" "blog" {
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
-
-  vpc id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
