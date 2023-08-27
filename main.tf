c65633eef9ec077da66a638758a8f59eb48c9028
edongsky <143239264+edongsky@users.noreply.github.com>
2023-08-27 20:26:43 +0800

    typo6

--- main.tf
+++ main.tf
@@ -39,7 +39,6 @@ module "autoscaling" {
   min_size = var.asg_min_size
   max_size = var.asg_max_size
 
-  vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
