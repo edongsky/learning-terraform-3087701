ad8bf483dd5838b2e29e0c8da7602fda1d1c0688
edongsky <143239264+edongsky@users.noreply.github.com>
2023-08-27 20:25:10 +0800

    typo5

--- main.tf
+++ main.tf
@@ -55,7 +55,6 @@ module "blog_alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = module.blog_sg.security_group_id
 
