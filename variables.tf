47c8ff892efcf09b27ab8835ddc61d6adf3ee58b
edongsky <143239264+edongsky@users.noreply.github.com>
2023-08-27 20:16:54 +0800

    fix typo again

--- variables.tf
+++ variables.tf
@@ -26,16 +26,16 @@ variable "environment" {
   })
 
   default = {
-  name = "dev"
+  name           = "dev"
   network_prefix = "10.0"
   }
 }
 
 variable "asg_min_size" {
   description = "Minimum number of instances in the ASG"
-  deafault    = 1
+  default     = 1
 }
 variable "asg_max_size" {
   description = "Maximum number of instances in the ASG"
-  default    = 2
+  default     = 2
 }
